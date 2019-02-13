c032cffec9db0b627b9b0f27a5e851652271dbee
Raul Quispe <raul.quispe@live.com>
2019-02-13 09:59:05 -0500

    Merge branch 'luisa' of https://github.com/Aquima/AppMVP-Android into kevin
    # Conflicts:
    #	app/src/main/res/layout/activity_register.xml

---
+++ app/src/main/java/com/tcs/app/Models/LogInModel.kt
@@ -0,0 +1,28 @@
+package com.tcs.app.Models
+
+import io.reactivex.Observable
+import java.util.regex.Pattern
+
+class LogInModel {
+    fun validateMail(email:String):Boolean{
+       return Pattern.compile(
+            "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                    + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                    + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+        ).matcher(email).matches()
+
+    }
+
+    fun validatePassword(password:String):Boolean{
+        if (password.length >= 6) {
+            return true
+        }else{
+            return false
+        }
+
+    }
+
+}
