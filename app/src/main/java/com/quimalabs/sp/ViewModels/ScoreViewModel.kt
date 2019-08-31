cebb7284c9ac3410173828213df5fc6acfeeeddc
JavierMed <josejavier.medina.huaraca@gmail.com>
2019-08-31 17:48:15 -0500

    scores

--- app/src/main/java/com/quimalabs/sp/ViewModels/ScoreViewModel.kt
+++ app/src/main/java/com/quimalabs/sp/ViewModels/ScoreViewModel.kt
@@ -1,7 +1,17 @@
 package com.quimalabs.sp.ViewModels
 
 import android.arch.lifecycle.ViewModel
+import io.reactivex.Observable
+
 
 class ScoreViewModel: ViewModel() {
+    fun returnScores() : Observable<List<String>>{
+        val observable:Observable<List<String>> = Observable.create { observer ->
+            val list =  listOf("1", "2", "3","4")
 
+            observer.onNext(list)
+        }
+        return observable
+
+    }
 }
