b5f14199fdff6a54db62a094b298eb6073c696c5
JavierMed <josejavier.medina.huaraca@gmail.com>
2019-09-04 18:30:36 -0500

    clickbuttons testPresent

--- app/src/main/java/com/quimalabs/sp/Views/TestPresentActivity.kt
+++ app/src/main/java/com/quimalabs/sp/Views/TestPresentActivity.kt
@@ -10,6 +10,13 @@ import android.widget.Button
 import com.quimalabs.sp.R
 import com.quimalabs.sp.ViewModels.TestPresentViewModel
 import kotlinx.android.synthetic.main.activity_test_present.*
+import android.view.ViewGroup
+
+
+
+
+
+
 
 class TestPresentActivity : AppCompatActivity() {
     lateinit var viewModel:TestPresentViewModel
@@ -17,6 +24,41 @@ class TestPresentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_present)
+    }
+    fun hereTestPresent(view: View){
+        this.btn_here_test_present.visibility = View.GONE
+        this.viewTestPresent.visibility = View.GONE
+        this.txt_1.visibility = View.GONE
+        this.txt_2.visibility = View.GONE
+        this.txt_scoreTest.visibility = View.GONE
+        this.linearLayoutTestPresent.visibility = View.GONE
+        clickButtons(view)
+    }
+    fun disableClickButtons(view: View){
+        this.btn_back.isClickable = false
+        this.switchPresent.isClickable = false
+        this.btn_refresh.isClickable = false
+        this.btn_bakSpace.isClickable = false
+
+        val viewgroup = view as ViewGroup
+        for (i in 0 until viewgroup.childCount) {
+            val v1 = viewgroup.getChildAt(i)
+            //if (v1 is ViewGroup) show_children(v1)
+            v1.isClickable = false
+        }
+    }
+    fun clickButtons(view: View){
+        this.btn_back.isClickable = true
+        this.switchPresent.isClickable = true
+        this.btn_refresh.isClickable = true
+        this.btn_bakSpace.isClickable = true
+
+        val viewgroup = view as ViewGroup
+        for (i in 0 until viewgroup.childCount) {
+            val v1 = viewgroup.getChildAt(i)
+            //if (v1 is ViewGroup) show_children(v1)
+            v1.isClickable = true
+        }
     }
 
     fun backPresent(view: View) {
